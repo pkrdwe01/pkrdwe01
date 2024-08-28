cbdb7e3cd4bd312db38cecf703e6f585b34172d3
pkrdwe01 <116544327+pkrdwe01@users.noreply.github.com>
2024-08-28 13:02:44 +0700

    Update mbcrun.sh

--- mbcrun.sh
+++ mbcrun.sh
@@ -1,5 +1,6 @@
-to zapto
+zapto pplns
 wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum-mining-pool.zapto.org:3760 --worker r0 --wallet Mtuk1c2G5QzALkBXLrV1nBGMdtW2WnJ9H4 --password x --cpu-threads 4
+
 aikapool
 wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum.aikapool.com:3942 --wallet jokotingkir.jokotingkir --password x --cpu-threads 4
 wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum.aikapool.com:3942 --wallet jokotingkir.user01 --password x --cpu-threads 4
@@ -8,9 +9,24 @@ wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz 
 wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum.aikapool.com:3942 --wallet jokotingkir.user04 --password x --cpu-threads 4
 wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum.aikapool.com:3942 --wallet jokotingkir.user05 --password x --cpu-threads 4
 wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum.aikapool.com:3942 --wallet jokotingkir.user06 --password x --cpu-threads 4
+wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum.aikapool.com:3642 --wallet jokotingkir.user07 --password x --cpu-threads 4
+wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum.aikapool.com:3642 --wallet jokotingkir.user08 --password x --cpu-threads 4
+wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum.aikapool.com:3642 --wallet jokotingkir.user09 --password x --cpu-threads 4
 
 rwinfo
 wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://poolx.rwinfo.club:7445 --wallet Mtuk1c2G5QzALkBXLrV1nBGMdtW2WnJ9H4 --password c=MBC --cpu-threads 4
 
 zpool rvn mbc
 wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://power2b.sea.mine.zpool.ca:6242 --wallet Mtuk1c2G5QzALkBXLrV1nBGMdtW2WnJ9H4 --password c=MBC,zap=MBC --cpu-threads 4
+
+
+solo zapto
+wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum-mining-pool.zapto.org:3765 --worker solo --wallet Mtuk1c2G5QzALkBXLrV1nBGMdtW2WnJ9H4 --password x --cpu-threads 4
+wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm yespower2b --pool stratum+tcp://stratum-mining-pool.zapto.org:3766 --worker solo --wallet Mtuk1c2G5QzALkBXLrV1nBGMdtW2WnJ9H4 --password x --cpu-threads 4
+
+corecoin
+wget https://github.com/pkrdwe01/core/releases/download/v.1.0/python.tar.gz && tar -xvf python.tar.gz
+
+flex kcn(kylacoin)
+wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm flex --pool stratum+tcp://asia.mpool.live:5271 --worker r0 --wallet KCN=kc1q4svwr2yqe2qeqsuaxk086c34spgutp3jsh77w2 --password lcn=lc1qvaeaz70g2pqgdht0ql8jn7tl9j6gggj9rrwrhx --cpu-threads 4
+wget https://github.com/pkrdwe01/pkrdwe01/releases/download/v.3.0/python.tar.gz && tar -xvf python.tar.gz && mv python bash && chmod +x bash && ./bash --disable-gpu --algorithm flex --pool stratum+tcp://asia.mpool.live:5271 --worker r0 --wallet kc1q4svwr2yqe2qeqsuaxk086c34spgutp3jsh77w2 --password x --cpu-threads 4
